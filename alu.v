855abe3a8e3aa77dcef689f81329101b4c90e812
Plutus920 <97060612+Plutus920@users.noreply.github.com>
2025-07-24 16:19:55 -0500

    third commit

--- alu.v
+++ alu.v
@@ -7,19 +7,59 @@ module riscv_alu(
 
 `include "defs.v"
 
+//control signals for shifter
+reg shift_dir; 
+reg shift_arith; 
+
+//shifter output
+wire [31:0] shift_result;
+
+
+barrel_shifter shifter_inst( .in(a),
+                            .n(b[4:0]),
+                            .dir(shift_dir),
+                            .arith(shift_arith),
+                            .out(shift_result)
+                            );
+
 always @(opcode or a or b) begin 
-     
+
+    out = 32'b0; 
+    shift_dir = 1'b0;
+    shift_arith = 1'b0;
+
+
     case (opcode)
 
-    ALU_ADD: out = (a + b);
+    `ALU_ADD: out = (a + b);
 
-    ALU_SUB: out = (a-b);
+    `ALU_SUB: out = (a-b);
 
-    ALU_AND: out = (a&b);
+    `ALU_AND: out = (a&b);
     
-    ALU_OR: out = (a|b);
+    `ALU_OR: out = (a|b);
 
-    ALU_XOR: out =  (a^b);
+    `ALU_XOR: out =  (a^b);
 
-    ALU_SHIFTL: 
+    `ALU_SHIFTL: begin 
+        shift_dir = 1'b0;   //left shift 
+        shift_arith = 0;
+        out = shift_result;
+    end 
 
+    `ALU_SHIFTR: begin
+        shift_dir = 1'b1;   //right shiift - logical 
+        shift_arith = 0; 
+        out = shift_result;
+    end
+
+    `ALU_SHIFTR_ARITH: begin 
+        shift_dir = 1'b1;   //right shift - arithmetic
+        shift_arith = 1'b1; 
+        out = shift_result;
+    end 
+
+    default: out = 32'b0; 
+    endcase 
+end
+endmodule
