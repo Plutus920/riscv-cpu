737ee85a9372128cde00124e63b0d114f6cc1e24
Plutus920 <97060612+Plutus920@users.noreply.github.com>
2025-08-26 21:59:10 -0500

    regfile bug

--- rtl/register_file.v
+++ rtl/register_file.v
@@ -6,18 +6,27 @@
                         input writeEnable,
                         input [4:0] writeAddr,
                         input [31:0] writeData,
-                        output reg [31:0] rs1_data,
-                        output reg [31:0] rs2_data );
+                        output wire [31:0] rs1_data,
+                        output wire [31:0] rs2_data );
 
     reg [31:0] register [31:0];
 
-    assign rs_1 = (readAddr1 == 0)? 32'b0 : register[readAddr1];
-    assign rs_2 = (readAddr2 == 0)? 32'b0 : register[readAddr2];
+    assign rs1_data = (readAddr1 == 0)? 32'b0 : register[readAddr1];
+    assign rs2_data = (readAddr2 == 0)? 32'b0 : register[readAddr2];
 
-    
+    integer i; 
+
     always @(posedge  clk) begin 
-        if(writeEnable && writeAddr != 5'b0) begin
+        
+        if (reset) begin
+            for (i=0; i<32; i=i+1) begin 
+                register[i] <= 32'b0; 
+            end
+        end
+
+        else if(writeEnable && writeAddr != 5'b0) begin
             register[writeAddr] <= writeData; 
+            $display("WRITE @ t=%0t: reg[%0d] <= 0x%h", $time, writeAddr, writeData);
         end
     end
  endmodule
--- tb/regfile_tb.v
+++ tb/regfile_tb.v
@@ -7,8 +7,8 @@ reg reset;
 reg [4:0] rs_1;
 reg [4:0] rs_2; 
 reg writeEn;
-reg [4:0] writeAddr; 
-reg [31:0] writeData; 
+reg [4:0] wDest; 
+reg [31:0] wData; 
 
 //regfile output 
 
@@ -22,16 +22,60 @@ register_file DUT(  .clk(clk),
                     .readAddr1(rs_1),
                     .readAddr2(rs_2),
                     .writeEnable(writeEn),
-                    .writeData(writeData),
+                    .writeAddr(wDest),
+                    .writeData(wData),
                     .rs1_data(rs1_data),
                     .rs2_data(rs2_data));
 
 
+initial clk = 0;
+always #5 clk = ~clk; 
 
-always #5 clk = ~clk;  
+//reset logic 
+initial begin 
+    reset = 1;
+    writeEn = 0;
+    wData = 0; 
+    wDest = 0;
+    #12 reset = 0; //reset deasserted at t=15 
+end
 
-task test_read_write;
-    input [4:0]
+
+initial begin 
+   $dumpfile("waveforms/regfile.vcd");
+   $dumpvars(0,regfile_tb);
+
+    @(negedge reset); //waits until reset 
+
+    writeEn = 1; 
+    wDest = 5'b00100;
+    wData = 32'h0000FFFF;
+    $display(">>> t=%0t | WRITE: x%d <= 0x%h", $time, wDest, wData);
+    #1;
+    @(posedge clk); //t=15 (first rising edge after reset)
+
+   
+    wDest = 5'b01000;
+    wData = 32'hFFFF0000;
+    $display(">>> t=%0t | WRITE: x%d <= 0x%h", $time, wDest, wData);
+    #1;
+    @(posedge clk); //t=25 (next rising edge)
+
     
+    writeEn = 0; 
+    @(posedge clk); //t=35 (next rising edge)
+    
+    rs_1 = 5'b00100; 
+    rs_2 = 5'b01000;
 
+    @(posedge clk); //t=45 (next rising edge)
+    #1;
 
+   // $display("writeEn = %b, wData = %h, rs1 = %h, rs2 = %h, rs1_data = %h, rs2_data = %h \n", writeEn, wData, rs_1, rs_2, rs1_data, rs2_data);
+    
+    $display("readAdd1 = %b, readAdd2 = %b, readData1 = %h, readData2 = %h\n", rs_1, rs_2, rs1_data, rs2_data); 
+
+    $finish;
+
+end
+endmodule
